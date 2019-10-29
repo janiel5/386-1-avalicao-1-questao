f521fdb56ea524ea8b12254051ef94300e6404c2
Janiel Sena <janielsena5@gmail.com>
2019-10-29 19:58:40 -0300

    modelo despesa

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class Despesa(models.Model):
+
+    data_criacao = models.DateField("data da criação")
+    CHOICE_TIPO_DESPESA = [("remedio", "Remédio"),
+                           ("roupas",'Roupas'),
+                           ("alimentacao","Alimentação"),
+                           ("educacao", "Educação"),
+                           ("transporte","Transporte"),
+                           ("outros","Outros")]
+    tipo_despesa = models.CharField("tipo de despesa",choices=CHOICE_TIPO_DESPESA, max_length=15)
+    descricao = models.TextField("descrição")
+    CHOICE_FORMA_PAGAMENTO = [("dinheiro","Dinheiro"),
+                              ("cartao_credito","cartão de crédito"),
+                              ("cartao_debito","cartão de débito"),
+                              ("cartao_crediario","cartão  crediário"),
+                              ("cheque","cheque")]
+    forma_pagamento = models.CharField("forma de pagamento",choices=CHOICE_FORMA_PAGAMENTO, max_length=15)
+    vencimento = models.DateField()
+    quitado = models.BooleanField()
+
+    ordering = ('forma_pagamento', 'vencimento')
+
+    def __str__(self):
+        return self.descricao
