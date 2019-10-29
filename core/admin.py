8b2e2dcfa56aada3390ab28dc803062422adc3e2
Janiel Sena <janielsena5@gmail.com>
2019-10-29 19:59:39 -0300

    instalação do modelo despesa

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from.models import *
 
 # Register your models here.
+class DespesaAdmin(admin.ModelAdmin):
+    list_display = ("data_criacao","tipo_despesa","descricao","forma_pagamento","vencimento","quitado")
+    list_filter =  ("quitado","vencimento")
+    date_hierarchy = "vencimento"
+
+admin.site.register(Despesa, DespesaAdmin)
+
